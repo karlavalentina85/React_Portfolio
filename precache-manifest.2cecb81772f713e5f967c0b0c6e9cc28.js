370558cace11dcef500ac3cf533729c476d2cf19
Blackbird <ksilva.rod@gmail.com>
2020-09-29 09:52:07 -0500

    Updates

---
+++ precache-manifest.2cecb81772f713e5f967c0b0c6e9cc28.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "187933f25e8eb5155daebc3b296df3c0",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "a0ee10fcbbfe0ea8a8b9",
+    "url": "/React_Portfolio/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c6c44fbd2b3263f1c28f",
+    "url": "/React_Portfolio/static/css/main.da97d9c0.chunk.css"
+  },
+  {
+    "revision": "a0ee10fcbbfe0ea8a8b9",
+    "url": "/React_Portfolio/static/js/2.47430860.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Portfolio/static/js/2.47430860.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6c44fbd2b3263f1c28f",
+    "url": "/React_Portfolio/static/js/main.67aea71f.chunk.js"
+  },
+  {
+    "revision": "e2df5e213b336240b9ac",
+    "url": "/React_Portfolio/static/js/runtime-main.728f603a.js"
+  },
+  {
+    "revision": "a16156632f24dcb2240ba1d804755a4e",
+    "url": "/React_Portfolio/static/media/portfolio.a1615663.jpg"
+  }
+]);
